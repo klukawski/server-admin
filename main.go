3e70e37fc08ade2d001289464517279f63bf7bc1
Kacper Łukawski <k.lukawski@polsource.com>
2017-12-07 13:12:40 +0100

    Switched to go-chi router and rewritten most of the code

--- main.go
+++ main.go
@@ -1,27 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/klukawski/server-admin/microservice"
+	"github.com/SermoDigital/jose/jws"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/docgen"
+	"github.com/klukawski/server-admin/auth"
 )
 
-func handleTest(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprint(w, "Hello World!")
-}
+var routes = flag.Bool("routes", false, "Generate router docs")
 
 func main() {
+	flag.Parse()
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	service := microservice.NewPanelMicroservice(":"+port, "keys/external.pub", "keys/local", "", "")
-	service.Endpoints["/test"] = handleTest
-	service.Start()
+	auth.Config.Validator = jws.NewValidator(map[string]interface{}{"iss": "panel"}, time.Minute, time.Minute, nil)
+	auth.Config.External = auth.LoadExternal("keys/external.pub")
+	auth.Config.Local = auth.LoadPrivate("keys/local")
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(auth.Auth)
+
+	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		println(w, "Hello World!")
+	})
+
+	if *routes {
+		fmt.Println(docgen.JSONRoutesDoc(r))
+		fmt.Println(docgen.MarkdownRoutesDoc(r, docgen.MarkdownOpts{
+			ProjectPath: "github.com/klukawski/server-admin",
+			Intro:       "This is a schema of server-admin's REST API",
+		}))
+		return
+	}
+
+	http.ListenAndServe(":"+port, r)
 }
